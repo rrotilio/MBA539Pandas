78509f0f22becfc35ae7d4f8d43543f9c25099fb
Ryan Rotilio <r.rotilio@gmail.com>
2018-04-07 17:05:13 -0400

    Initial commit

---
+++ P1.py
@@ -0,0 +1,13 @@
+print("Twinkle, twinkle, little star, How I wonder what you are!")
+print("Up above the world so high, Like a diamond in the sky.")
+print("Twinkle, twinkle, little star, How I wonder what you are")
+print("")
+print('Twinkle, twinkle, little star, How I wonder what you are!\nUp above the world so high, Like a diamond in the sky.\nTwinkle, twinkle, little star, How I wonder what you are')
+
+song="""
+Twinkle, twinkle, little star, How I wonder what you are!
+Up above the world so high, Like a diamond in the sky.
+Twinkle, twinkle, little star, How I wonder what you are
+"""
+
+print(song)
